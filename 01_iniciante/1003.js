b9f7fd8c8a5a9b505999f3c1546e635bb1854e89
warleyramires <warleyramiresgoncalves@gmail.com>
2022-09-10 01:04:14 -0300

    Exercícios JS

---
+++ 01_iniciante/1003.js
@@ -0,0 +1,20 @@
+// SOMA SIMPLES
+
+// Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+
+// Entrada
+// O arquivo de entrada contém 2 valores inteiros.
+
+// Saída
+// Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+const prompt = require('prompt-sync')({sigint:true})
+
+let a, b, soma;
+
+a = Number(prompt());
+b = Number(prompt());
+
+soma = a + b;
+
+console.log(`SOMA = ${soma}\n`);
---
+++ 01_iniciante/1004.js
@@ -0,0 +1,20 @@
+// PRODUTO SIMPLES
+
+// Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+
+// Entrada
+// O arquivo de entrada contém 2 valores inteiros.
+
+// Saída
+// Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+
+const prompt = require('prompt-sync')({sigint: true})
+
+let a, b, prod;
+
+a = Number(prompt());
+b = Number(prompt());
+
+prod = a * b;
+
+console.log(`PROD = ${prod}\n`)
